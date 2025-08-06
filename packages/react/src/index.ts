5dea9980acfc5f64e72f65215a6fe46ff221d8ad
Dmitry Ponomarev <demdxx@gmail.com>
2025-08-06 15:31:36 +0200

    Up to date CI workflow with dry-run publishing and package verification

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,36 +1,27 @@
 import AdViewUnitClient from './AdViewUnit/AdViewUnit.client';
 // import AdViewUnitServer from './AdViewUnit/AdViewUnit.server';
 import AdViewProvider from './AdViewUnit/AdViewProvider';
-import AdViewUnitTypeSwitch from './AdViewUnit/AdViewUnitTypeSwitch';
 import AdViewUnitBanner from './AdViewUnit/AdViewUnitBanner';
 import AdViewUnitNative from './AdViewUnit/AdViewUnitNative';
 import AdViewUnitProxy from './AdViewUnit/AdViewUnitProxy';
+import AdViewUnitTypeSwitch from './AdViewUnit/AdViewUnitTypeSwitch';
 // import AdViewUnitTracking from './AdViewUnit/AdViewUnitTracking';
 // import AdViewUnitWrapperClient from '@/src/AdViewUnit/AdViewUnitWrapper.client';
 
 const AdView = {
-  AdViewUnitClient,
-  AdViewProvider,
-  AdViewUnitTypeSwitch,
-  AdViewUnitBanner,
-  AdViewUnitNative,
-  AdViewUnitProxy,
-  // AdViewProvider,
-  // AdViewUnitServer,
-  // AdViewUnitTracking,
-  // AdViewUnitWrapperClient,
+  UnitClient: AdViewUnitClient,
+  Provider: AdViewProvider,
+  UnitTypeSwitch: AdViewUnitTypeSwitch,
+  UnitBanner: AdViewUnitBanner,
+  UnitNative: AdViewUnitNative,
+  UnitProxy: AdViewUnitProxy,
 };
 
 export {
+  AdViewProvider, AdViewUnitBanner,
   // AdViewUnitServer,
-  AdViewUnitClient,
-  AdViewProvider,
-  AdViewUnitTypeSwitch,
-  AdViewUnitBanner,
-  AdViewUnitNative,
-  AdViewUnitProxy,
-  // AdViewUnitTracking,
-  // AdViewUnitWrapperClient,
+  AdViewUnitClient, AdViewUnitNative,
+  AdViewUnitProxy, AdViewUnitTypeSwitch
 };
 
 export default AdView;
