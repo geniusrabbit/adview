51da4cdec8d005a9ba8a7d6d1b857f9d34764b8b
derek-heycar <derekgoha@rambler.ru>
2025-08-28 08:50:05 +0200

    improve popunder package as a module to import

--- packages/react-popunder/src/AdPopunder.tsx
+++ packages/react-popunder/src/AdPopunder.tsx
@@ -1,14 +1,14 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { AdPopunderProps } from './types';
 import PopUnder from '@adview/popunder';
 
-const AdPopunder: React.FC<AdPopunderProps> = ({ config }) => {
+function AdPopunder({ ...config }: AdPopunderProps) {
   useEffect(() => {
     // @ts-ignore
     new PopUnder(config);
   }, []);
 
   return null;
-};
+}
 
 export default AdPopunder;
--- packages/react-popunder/src/types.ts
+++ packages/react-popunder/src/types.ts
@@ -1,5 +1,13 @@
-import { PopUnderData } from '@adview/popunder';
-
 export interface AdPopunderProps {
-  config: PopUnderData;
+  [key: string]: any;
+  'cookie-name'?: string;
+  'cookie-domain'?: string;
+  every?: string;
+  'every-direct'?: number;
+  'ignore-filter'?: string;
+  target?: string;
+  categories?: string;
+  template?: string;
+  unitid?: string;
+  mode?: string;
 }
